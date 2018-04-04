547aad7942f2678bc0610a45aa795a9b1b67b1b1
GussRw <gussrw1@gmail.com>
2018-04-03 21:32:42 -0500

    Bluethoth name was changed

--- sensor_ultrasonico/sensor_ultrasonico.ino
+++ sensor_ultrasonico/sensor_ultrasonico.ino
@@ -1,20 +1,26 @@
 #include <NewPing.h>
 NewPing sonar(12, 11, 200);
  
-void setup() {
-  Serial.begin(9600);
-  pinMode(13,OUTPUT);
+void setup() 
+{
+    Serial.begin(9600);
+    delay(1000);
+    Serial.println("AT+NAMESmartCane");
+    pinMode(3,OUTPUT);
 }
  
-void loop() {
-  delay(1000);
-  int distance = sonar.ping_median()/US_ROUNDTRIP_CM;
-  Serial.print("Distancia: ");
-  Serial.print(distance);
-  Serial.println("cm");
-  if(distance<5){
-    analogWrite(13,20);
-    delay(100);
-    analogWrite(13,0);
+void loop() 
+{
+    delay(1000);
+    int distance = sonar.ping_median() / US_ROUNDTRIP_CM;
+    Serial.print("Distancia: ");
+    Serial.print(distance);
+    Serial.println("cm");
+    
+    if(distance<5)
+    {
+      analogWrite(3,20);
+      delay(100);
+      analogWrite(3,0);
     }
 }
